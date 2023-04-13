a32724db21981e6145f7e9470f803e5dd62a8a2a
Abby Wadsworth <abbywadsworth27.5@gmail.com>
2023-04-13 17:05:39 -0600

    trying something

--- code.py
+++ code.py
@@ -73,9 +73,9 @@ colours = {
     
     
     
-fig = px.scatter(df_gen[df_gen['Generation']==selected_gen],
-                 x='Weight',y='Height', color_discrete_map=colours,
-                 hover_data='Name', size='HP')
+fig = px.scatter(df_gen,
+                 x='Weight',y='Height', color='Primary'
+                 color_discrete_map=colours, hover_data='Name', size='HP')
 fig.update_layout(title=f'Weight vs Height for {selected_type} in Generation {selected_gen}')
 
 #telling streamlit you have a plotly fig to insert
