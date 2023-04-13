5ab7a183fa1e3e7ee0f4f49d57345a2c12fa590f
Abby Wadsworth <abbywadsworth27.5@gmail.com>
2023-04-13 16:42:43 -0600

    fixing things

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ import streamlit as st
 df = pd.read_csv('pokemonclean.csv')
 
 #getting rid of percentage_male
-df.drop(labels='percentage_male', axis=1, inplace=True)
+#df.drop(labels='percentage_male', axis=1, inplace=True)
 
 
 
@@ -25,7 +25,7 @@ types.append('All')
 
 
 
-years = list(df['Year'].unique())
+#years = list(df['Year'].unique())
 
 # Define the Streamlit app
 st.title('Graph your own Pokemon data')
@@ -40,7 +40,7 @@ else:
     df_gen = df[df['Generation'] == selected_gen]
 
 fig = px.scatter(df_gen[df_gen['Generation']==selected_gen],
-                 x='Weight',y='Height', color='Region',
+                 x='Weight',y='Height',
                  hover_data=['Name'], size='HP')
 fig.update_layout(title=f'Weight vs Height for {selected_type} in Generation {selected_gen}')
 
